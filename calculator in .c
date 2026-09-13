c39ff574d084620a422c05e5017131e30c8fb64a
Vishal Rai <tumhara-email@example.com>
2026-09-13 16:28:01 +0530

    calculator in c

---
+++ calculator in .c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main() {
+    double num1, num2, result;
+    char op;
+
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &op);
+
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+            printf("Result = %.2lf\n", result);
+            break;
+
+        case '-':
+            result = num1 - num2;
+            printf("Result = %.2lf\n", result);
+            break;
+
+        case '*':
+            result = num1 * num2;
+            printf("Result = %.2lf\n", result);
+            break;
+
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                printf("Result = %.2lf\n", result);
+            } else {
+                printf("Error: Division by zero!\n");
+            }
+            break;
+
+        default:
+            printf("Invalid operator!\n");
+    }
+
+    return 0;
+}
